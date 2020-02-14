8913a4e325fd84bdf8fcb8adec5554b8e2b42b8e
Joel Krause <joel@raak.com.au>
2020-02-14 16:00:02 +1100

    moved footer into page component
    for smoother page transition animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,15 @@
   <div>
     <Header />
     <nuxt />
-    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
-import Footer from '~/components/Footer.vue'
 
 export default {
   components: {
-    Header,
-    Footer
+    Header
   }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,39 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        nuxt-text
-      </h1>
-      <h2 class="subtitle">
-        Testing Nuxt.js
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main>
+    <div class="container">
+        <h1 class="title">
+          nuxt-text
+        </h1>
+        <h2 class="subtitle">
+          Testing Nuxt.js
+        </h2>
+        <div class="links">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            class="button--green"
+          >
+            Documentation
+          </a>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            class="button--grey"
+          >
+            GitHub
+          </a>
+        </div>
       </div>
-    </div>
-  </div>
+    <Footer />
+  </main>
 </template>
+
+<script>
+import Footer from '~/components/Footer.vue'
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
