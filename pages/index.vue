7f7cfb6900f42aad8793470bf09fbd5bc4b5df26
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2020-02-15 15:57:36 +1100

    redirect to /home route

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,9 @@
 <template>
-  <main>
-    <div class="container">
-        <h1 class="title">
-          nuxt-text
-        </h1>
-        <h2 class="subtitle">
-          Testing Nuxt.js
-        </h2>
-<div class="page" v-for="page in pages" :key="page.id">
-      <h3>
-      <!-- for each one of them, we’ll render their title, and link off to their individual page -->
-        <a :href="`${page.slug}`">{{ page.title.rendered }}</a>
-      </h3>
-      <a :href="`${page.slug}`" class="readmore">Read more ⟶</a>
-    </div>
-      </div>
-    <Footer />
-  </main>
+
 </template>
 
 <script>
-import Footer from '~/components/Footer.vue'
-
 export default {
-  components: {
-    Footer
-  },
-computed: {
-  pages() {
-    return this.$store.state.pages;
-  },
- },
-created() {
-  this.$store.dispatch("getPages");
-},
-};
+  middleware: 'redirect'
+}
 </script>
